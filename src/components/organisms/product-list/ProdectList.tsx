d6063139d734ced0ba5d2c69b02a29653f822a1b
Hirano <hnaoya1120@gmail.com>
2023-11-13 23:58:37 +0900

    headerにカートアイコン追加

--- src/components/organisms/product-list/ProdectList.tsx
+++ src/components/organisms/product-list/ProdectList.tsx
@@ -9,6 +9,7 @@ import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
 const useStyles = makeStyles((theme) => ({
     icon: {
@@ -107,18 +108,12 @@ export const ProductList = () => {
                                         >
                                             Heading
                                         </Typography>
-                                        <Typography>
-                                            This is a media card. You can use
-                                            this section to describe the
-                                            content.
-                                        </Typography>
+                                        <Typography>$18:00</Typography>
                                     </CardContent>
                                     <CardActions>
-                                        <Button size="small" color="primary">
-                                            View
-                                        </Button>
-                                        <Button size="small" color="primary">
-                                            Edit
+                                        <Button variant="outlined">
+                                            <ShoppingCartIcon />
+                                            カートへ
                                         </Button>
                                     </CardActions>
                                 </Card>
